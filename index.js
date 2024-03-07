cb4d5e518bb4614b080b213815b34344a31321f4
Kartik Kankurte <kartikkankurte@gmail.com>
2024-03-07 21:43:34 +0530

    changed api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const country = 'in'; 
-const apiKey = '74234f2e15444d4b88179f7ecb54d5cb'; 
-const url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`;
+const apiKey = '36d36447a72d420463c5a78ba3142205'; 
+const url = `https://gnews.io/api/v4/search?q=example&lang=en&country=${country}&max=6&apikey=${apiKey}`;
+
 
 const newsContainer = document.getElementById('news-container');
 
